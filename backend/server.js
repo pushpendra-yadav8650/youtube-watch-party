12cac1c587ecbec3cd6eafce1722c8b0e6b0e056
Pushpendra yadav <pushpendrayadav8434@gmail.com>
2026-09-12 17:11:42 +0530

    update backend

--- backend/server.js
+++ backend/server.js
@@ -11,17 +11,23 @@ const roomRoutes = require("./routes/roomRoutes");
 const registerSocketHandlers = require("./socket/socketHandler");
 
 //Connect Frontend to backend  using CORS
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "https://youtube-watch-party-2-7698.onrender.com";
+
 
 const app = express();
 const server = http.createServer(app);
 
 // Socket.IO server, allowed to receive connections from our frontend origin.
+// const io = new Server(server, {
+//   cors: { origin: CLIENT_ORIGIN, methods: ["GET", "POST"] },
+// });
 const io = new Server(server, {
-  cors: { origin: CLIENT_ORIGIN, methods: ["GET", "POST"] },
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
-
 // Middleware
 app.use(cors({ origin: CLIENT_ORIGIN }));
 app.use(express.json());
